7b42a0794be6d7101d3a043ed449154d184acfcd
renzodamian <renzojaramillocueva@gmail.com>
2023-10-20 11:26:17 -0500

    update Patient

--- src/main/java/com/rdjaramillo/model/Patient.java
+++ src/main/java/com/rdjaramillo/model/Patient.java
@@ -10,6 +10,11 @@ import lombok.NoArgsConstructor;
 public class Patient {
 
     private Integer idPatient;
-
     private String firstName;
+    private String lastName;
+    private String dni;
+    private String address;
+    private String phone;
+    private String email;
 }
+
